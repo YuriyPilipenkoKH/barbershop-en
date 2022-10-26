40c362d03db18301b9b86956e3de47363d79908e
Yuriy Pilipenko <yurik2061@gmail.com>
2022-10-26 08:57:44 +0300

    в

---
+++ code.js
@@ -0,0 +1,7 @@
+// Change code below this line
+const orderedQuantity = 6;
+const pricePerDroid = 800;
+const deliveryFee = 50;
+const totalPrice = 4850;
+const message = `You ordered droids worth ${((orderedQuantity * pricePerDroid) + deliveryFee)} credits.
+Delivery (${deliveryFee} credits) is included in total price.`;
